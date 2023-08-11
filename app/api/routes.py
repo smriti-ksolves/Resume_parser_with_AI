41ba427611b58df9fa1aa10c8a97a00bf8567823
Smriti <smriti.tiwari@ksolves.com>
2023-08-11 13:20:57 +0530

    uncommented cors and change the params get method

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-# from flask_cors import cross_origin
+from flask_cors import cross_origin
 import json
 from app.models.sign_up import signup_user
 from app.models.sign_in import signin_user
@@ -10,7 +10,7 @@ api_routes = Blueprint('api', __name__)
 
 # Define your routes using the blueprint
 @api_routes.route('/')
-# @cross_origin(supports_credentials=False)
+@cross_origin(supports_credentials=False)
 def index():
     data = {
         "message": "Server Started ...",
@@ -20,25 +20,25 @@ def index():
 
 @api_routes.route('/signup', methods=['POST'])
 def signup():
-    # params = json.loads(request.json.decode("utf-8"))
-    params = request.json
+    params = json.loads(request.json.decode("utf-8"))
+    # params = request.json
     response = signup_user(params)
     return jsonify(response)
 
 
 @api_routes.route('/signin', methods=['POST'])
 def signin():
-    # params = json.loads(request.json.decode("utf-8"))
-    params = request.json
+    params = json.loads(request.json.decode("utf-8"))
+    # params = request.json
     response = signin_user(params)
     return jsonify(response)
 
 
 @api_routes.route('/get_parsed_data', methods=['POST'])
 def resume_parsing():
-    # params = json.loads(request.json.decode("utf-8"))
+    params = json.loads(request.json.decode("utf-8"))
     # files = request.files['files']
-    params = request.json
+    # params = request.jsong
     data = get_extracted_data(params)
     response = resume_data_create(params, data)
     return jsonify(response)
--- app/models/data_parser.py
+++ app/models/data_parser.py
@@ -3,6 +3,7 @@ import openai
 from dotenv import load_dotenv
 import regex
 import time
+import re
 
 pattern = regex.compile(r'\{(?:[^{}]|(?R))*\}')
 my_keys = ['name', 'email_id', 'phone_no', 'total_experience', 'current_employer', 'current_designation', 'skills',
@@ -14,47 +15,50 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def Data_Parser(data):
     try:
-        prompt = f'''Please extract the following details from the provided resume text and present them in Python JSON format:
-        - Name
-        - Email ID
-        - Phone Number
-        - Location
-        - Total Experience
-        - Current Employer (Identify the candidate's most recent workplace, focusing on keywords like "Current Employer," "Recent Job," or "Present Company")
-        - Current Designation
-        - Skills
-        - Current Employer Project Skills
-        - URLs ID
-        - College Finished Year
-        
-        For each detail, please use the following keys in the JSON:
-        - Name: name (string)
-        - Email ID: email_id (string)
-        - Phone Number: phone_no (string)
-        - Location: location (string)
-        - Total Experience: total_experience (integer in years)
-        - Current Employer: current_employer (string)
-        - Current Designation: current_designation (string)
-        - Skills: skills (list of strings)
-        - Current Employer Project Skills: current_skills (list of strings)
-        - URLs ID: urls (list of strings)
-        - College Finished Year: college_finished_year (string)
-        Please note that the resume may have formatting variations that affect text extraction. To address this:
-        
-        1.  perform text cleaning to remove unwanted characters, extra spaces, and line breaks.
-        2. Utilize regular expressions (regex) to match patterns for fields like names, email IDs, phone numbers, etc., considering possible variations.
-        
-        Feel free to experiment with your parsing logic and test it on various resumes to improve accuracy. Avoid including any comment or code in the JSON output.
-        
-        For the "name" field, please employ advanced text processing techniques to identify patterns resembling names, even if they are presented in separate characters or unusual formats. Regular expressions or pattern matching can be useful for this purpose.
-        
-        To identify the current employer, focus on sections titled "Work Experience," "Employment History," or similar headings. Avoid including any comments in the JSON output. Provide only the JSON data.
-        
-                
-        Resume:-
-        
-        {data[:6000]}
+        prompt = f'''
+            Please extract the following details from the provided resume text and present them in Python JSON format:
+            - Name
+            - Email ID
+            - Phone Number
+            - Location
+            - Total Experience (expressed in years)
+            - Current Employer (Identify the candidate's most recent workplace, focusing on keywords like "Current Employer," "Recent Job," or "Present Company")
+            - Current Designation
+            - Skills
+            - Current Employer Project Skills
+            
+            For each detail, please use the following keys in the JSON:
+            - Name: name (string)
+            - Email ID: email_id (string)
+            - Phone Number: phone_no (string)
+            - Location: location (string)
+            - Total Experience: total_experience (integer in years)
+            - Current Employer: current_employer (string)
+            - Current Designation: current_designation (string)
+            - Skills: skills (list of strings)
+            - Current Employer Project Skills: current_skills (list of strings)
+            
+            To improve the accuracy of extracting the "name" field, follow these strategies:
+                1. Search for sections or lines that may contain personal information, such as "Personal Details," "Contact Information," or "Profile."
+                2. Extract the text from these sections and analyze patterns that typically represent names.
+                3. Use regular expressions to match common name patterns, while considering the presence of special characters and diacritics.
+                4. Normalize Unicode characters (e.g., NFKD) to handle diacritics and special characters effectively.
+           To improve the accuracy of extracting the "Current Employer," follow these strategies:
+                1. Locate sections or headings related to work experience, such as "Work History," "Employment," or "Professional Experience."
+                2. Extract the text within these sections and capture the surrounding context, including job titles and date ranges.
+                3. Search for specific keywords like "Current Employer," "Present Company," or "Recent Job" within these sections.
+                4. Account for formatting variations using regular expressions to match potential variations of the keywords.
+                5. Be cautious with keywords like "LinkedIn" which might be present in contexts other than current employment. Consider filtering out these false positives by checking adjacent job titles and date ranges.
+                6. Utilize multiple context clues, such as adjacent job titles and date ranges, to accurately identify the current employer.
+            When identifying the current employer, focus on sections explicitly related to work experience. Exclude comments in the JSON output. Provide only JSON data.
+            
+            Resume:-
+            
+            {data[:6000]}
 
+
+
+        
         '''
 
         def make_openai_request():
@@ -84,14 +88,20 @@ def Data_Parser(data):
         return msg
 
 
+def clean_name(name):
+    cleaned_name = re.sub(r'[^a-zA-Z\s\.\-]', '', name)
+    return cleaned_name.strip()
+
+
 def response_validation(response):
     data = {}
     try:
         data = eval(pattern.findall(response)[0])
+        if data["name"]:
+            data["name"] = clean_name(data["name"])
+
         filtered_data = {key: data[key] for key in my_keys}
 
         return filtered_data
     except Exception as err:
         return data
-
-
